4dfe22e069b4d349f94674294b9fba0634b11191
EngMohamedKhalid <engmohamedkhalid4404@gmail.com>
2024-07-02 19:38:11 +0300

    add home

---
+++ lib/features/home_feature/presentation/screens/home_screen.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:kolayca_app/app/widgets/image_widget.dart';
+import 'package:kolayca_app/app/widgets/text_widget.dart';
+import 'package:kolayca_app/features/home_feature/presentation/widgets/custom_home_item.dart';
+
+import '../../../../app/utils/app_colors.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          ImageWidget(
+              imageUrl: "assets/images/home.png",
+            width: double.infinity,
+            height: 300.h,
+            fit: BoxFit.fill,
+          ),
+          16.verticalSpace,
+          Expanded(
+            child: GridView.builder(
+              itemCount: 20,
+                padding: EdgeInsets.symmetric(horizontal: 16.w),
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    crossAxisSpacing: 8,
+                    mainAxisSpacing: 8,
+                ),
+                itemBuilder: (context, index) {
+                  return const CustomHomeItem();
+                }
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/features/home_feature/presentation/widgets/custom_home_item.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:kolayca_app/app/widgets/image_widget.dart';
+import 'package:kolayca_app/app/widgets/text_widget.dart';
+
+import '../../../../app/utils/app_colors.dart';
+class CustomHomeItem extends StatelessWidget {
+  const CustomHomeItem({super.key, this.name, this.image});
+  final String? name;
+  final String? image;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 200.w,
+      height: 200.h,
+      padding: EdgeInsets.all(8.sp),
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(16),
+          color: AppColors.mainColor,
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.5),
+              spreadRadius: 5,
+              blurRadius: 7,
+              offset: const Offset(0, 3), // changes position of shadow
+            ),
+          ]
+      ),
+      child: Column(
+        children: [
+          Expanded(
+            child: ImageWidget(
+              imageUrl: image??"assets/images/img.png",
+              // width: double.infinity,
+              // height: 300.h,
+              // fit: BoxFit.fill,
+            ),
+          ),
+          TextWidget(
+            title: name??"تحدث مع الاتراك",
+            titleSize: 20.sp,
+            titleColor: AppColors.white,
+            titleFontWeight: FontWeight.bold,
+          ),
+        ],
+      ),
+
+    );
+  }
+}
